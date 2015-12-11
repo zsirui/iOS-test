bc53497c45999129d476c26bc78be256c633205d
Ray <zhengsirui@suanier.com>
2015-12-11 18:52:54 +0800

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Ray on 12/11/15.
+//  Copyright (c) 2015 Ray. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
